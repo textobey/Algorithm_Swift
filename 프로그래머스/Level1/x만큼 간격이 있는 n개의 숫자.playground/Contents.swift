60b4c9986736b6a5c45e45e492ec0a083f048311
textobey <dogsan_e@naver.com>
2023-05-08 20:07:21 +0900

    [ADD] Remind Day1.

---
+++ 프로그래머스/Level1/x만큼 간격이 있는 n개의 숫자.playground/Contents.swift
@@ -0,0 +1,31 @@
+import UIKit
+
+// 문제 설명
+// 함수 solution은 정수 x와 자연수 n을 입력 받아, x부터 시작해 x씩 증가하는 숫자를 n개 지니는 리스트를 리턴해야 합니다. 다음 제한 조건을 보고, 조건을 만족하는 함수, solution을 완성해주세요.
+
+// 제한 조건
+// x는 -10000000 이상, 10000000 이하인 정수입니다.
+// n은 1000 이하인 자연수입니다.
+
+// 입출력 예
+// x    n    answer
+// 2    5    [2,4,6,8,10]
+// 4    3    [4,8,12]
+// -4    2    [-4, -8]
+
+func solution(_ x: Int, _ n: Int) -> [Int64] {
+    guard x != 0 else { return Array(repeating: 0, count: n) }
+    
+    var result: [Int64] = []
+    
+    for i in stride(from: x, through: x * n, by: x) {
+        result.append(Int64(i))
+    }
+    
+    return result
+}
+
+solution(0, 0)
+solution(0, 1)
+solution(4, 3)
+solution(-4, 2)
---
+++ 프로그래머스/Level1/정수 내림차순으로 배치하기.playground/Contents.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+// 문제 설명
+// 함수 solution은 정수 n을 매개변수로 입력받습니다. n의 각 자릿수를 큰것부터 작은 순으로 정렬한 새로운 정수를 리턴해주세요. 예를들어 n이 118372면 873211을 리턴하면 됩니다.
+
+// 제한 조건
+// n은 1이상 8000000000 이하인 자연수입니다.
+
+// 입출력 예
+// n    return
+// 118372    873211
+
+func solution(_ n: Int64) -> Int64 {
+    let result = String(n).map { Int(String($0))! }
+        .sorted(by: >)
+        .map { String($0) }
+        .joined()
+    
+    return Int64(result)!
+}
+
+solution(118372)
