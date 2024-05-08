b4ce000f9e6e9985b761b0587cb444ad0a239d14
textobey <dogsan_e@naver.com>
2024-05-08 22:53:39 +0900

    [FIX] 브루트포스 알고리즘

--- 백준/Baekjoon_Roadmap/Baekjoon_Roadmap/BruteForce/2309_일곱난쟁이.swift
+++ 백준/Baekjoon_Roadmap/Baekjoon_Roadmap/BruteForce/2309_일곱난쟁이.swift
@@ -8,33 +8,49 @@
 import Foundation
 
 // 2309_일곱난쟁이
-// needFix.
+
+// 내가 놓친 아이디어.
+// 나는 7명을 합해서 딱 100이 되는 경우의수를 찾으려고 했으나,
+// 모든 키를 더한 값에서 100을 빼고,
+// 배열에서 2개 값을 뽑는 모든 경우의 수를 체크하여
+// 초과된 키와 같은 결과를 가지는 난쟁이 2명을 찾는게 훨씬 빠르다.
 
 func solution() {
     var heights: [Int] = Array(repeating: 0, count: 9)
-    
+
     for i in 0 ..< 9 {
         let height = Int(readLine()!)!
         heights[i] = height
     }
     
-    loop: for i in 0 ..< 9 {
-        var temp: Int = 0
-        var result: [Int] = []
-        
-        for j in 0 ..< 9 {
-            if i != j {
-                temp += heights[j]
-                result.append(heights[j])
-            }
-            if temp == 100 && result.count == 7 {
-                print(result)
+//    var heights: [Int] = [
+//        20,
+//        7,
+//        23,
+//        19,
+//        10,
+//        15,
+//        25,
+//        8,
+//        13
+//    ]
+    
+    let overHeight: Int = heights.reduce(0, +) - 100
+    var fakers: Set<Int> = []
+    
+    loop: for i in 0 ..< heights.count {
+        for j in (i + 1) ..< 9 {
+            if (heights[i] + heights[j]) == overHeight {
+                fakers.insert(heights[i])
+                fakers.insert(heights[j])
                 break loop
             }
         }
     }
     
-    //print(heights)
+    Set(heights).subtracting(fakers).sorted().forEach {
+        print($0)
+    }
 }
 
 solution()
