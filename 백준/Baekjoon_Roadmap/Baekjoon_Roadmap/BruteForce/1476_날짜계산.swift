9c9305c9d2e99683b14c921f5120a83efa156269
textobey <dogsan_e@naver.com>
2024-05-20 22:48:53 +0900

    [MOD] 브루트포스

--- 백준/Baekjoon_Roadmap/Baekjoon_Roadmap/BruteForce/1476_날짜계산.swift
+++ 백준/Baekjoon_Roadmap/Baekjoon_Roadmap/BruteForce/1476_날짜계산.swift
@@ -8,13 +8,20 @@
 import Foundation
 
 // 1476_날짜계산
+//
+// 아이디어1.
+// 뭐랄까..전형적인 브루트포스 스탠다드 문제인거 같다.
+// while 루프를 계속 돌다보면 E, S, M의 범위는 각자 다르지만
+// 입력된 E, S, M과 일치하는 순간이 있도록 보장되는 문제
+// 1년이 지날때마다 카운트 해주면서 그 순간이 언제인지 출력하면됨
+// 단, 1년부터 시작하기에 1, 1, 1로 초기값 설정
 
 func solution() {
-    //let input = readLine()!.split(separator: " ").map { Int(String($0))! }
+    let input = readLine()!.split(separator: " ").map { Int(String($0))! }
     
-    var E: Int = 15
-    var S: Int = 28
-    var M: Int = 19
+    let E: Int = input[0]
+    let S: Int = input[1]
+    let M: Int = input[2]
     
     var earth: Int = 1
     var sun: Int = 1
